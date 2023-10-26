4c5cf05322fd93b603b23ad3228de35e9e91c8aa
yihsintsai1003 <127736452+yihsintsai1003@users.noreply.github.com>
2023-10-26 12:50:24 +0800

    Add files via upload

---
+++ fir.c
@@ -0,0 +1,26 @@
+#include "fir.h"
+
+void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
+	//initial your fir
+	for (int index = 0; index < N; index++)
+	{
+		inputbuffer[index] = inputsignal[index];
+    outputsignal[index] = 0;
+	}
+	
+}
+
+int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
+	initfir();
+	//write down your fir
+	for (int i = 0; i < N; i++)
+	{
+   	for (int j = 0; j < i; j++)
+    { 
+    outputsignal[i] += taps[i-j]*inputbuffer[j];
+   
+	}
+  } 
+	return outputsignal;
+}
+		
